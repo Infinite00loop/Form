6868394118fbf1cd9e684f532585f33d228c6fcd
Infinite00loop <samriddhisingh07092002@gmail.com>
2023-10-02 15:11:17 +0530

    form

---
+++ form.js
@@ -0,0 +1,8 @@
+function onsignup(){
+    console.log(document.getElementById('id1').value);
+    console.log(document.getElementById('id2').value);
+    console.log(document.getElementById('id3').value);
+    console.log(document.getElementById('id4').value);
+    console.log(document.getElementById('id5').value);
+
+     }
