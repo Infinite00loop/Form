ffd23b730705a7bfa48d22f308840c89cf83c391
Infinite00loop <samriddhisingh07092002@gmail.com>
2023-10-02 15:41:38 +0530

    form

--- form.js
+++ form.js
@@ -1,8 +1,13 @@
 function onsignup(){
-    console.log(document.getElementById('id1').value);
-    console.log(document.getElementById('id2').value);
-    console.log(document.getElementById('id3').value);
-    console.log(document.getElementById('id4').value);
-    console.log(document.getElementById('id5').value);
+    localStorage.setItem('Name' ,document.getElementById('id1').value );
+    localStorage.setItem('Email' ,document.getElementById('id2').value );
+    localStorage.setItem('Phone' ,document.getElementById('id3').value );
+    localStorage.setItem('Date' ,document.getElementById('id4').value );
+    localStorage.setItem('Time' ,document.getElementById('id5').value );
+    console.log(localStorage.getItem('Name'));
+    console.log(localStorage.getItem('Email'));
+    console.log(localStorage.getItem('Phone'));
+    console.log(localStorage.getItem('Date'));
+    console.log(localStorage.getItem('Time'));
 
      }
