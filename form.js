7d63e2bc8d215094c1ff0229978cad32a8863e38
Infinite00loop <samriddhisingh07092002@gmail.com>
2023-10-25 20:45:11 +0530

    saving data on cloud

--- form.js
+++ form.js
@@ -11,12 +11,19 @@ function onsignup(){
         phone: phone_
     };
 
-    var myObjSerial=JSON.stringify(myObj);
-    localStorage.setItem(email_,myObjSerial);
+    // var myObjSerial=JSON.stringify(myObj);
+    // localStorage.setItem(email_,myObjSerial);
+    axios.post('https://crudcrud.com/api/0a3d0f6876dc4abaa5721543c25c5d21/appointments',myObj)
+    .then((res)=> console.log(res))
+    .catch((err)=> console.log(err)); 
 
-    
+    showData(myObj);
+
+}
+
+function showData(obj){
     var newList=document.createElement('li');
-    var text=name_+" - "+email_+" - "+phone_+" - ";
+    var text=obj.name+" - "+obj.email+" - "+obj.phone+" - ";
     newList.appendChild(document.createTextNode(text));
     var delButton=document.createElement('button');
     delButton.className='delete';
@@ -29,27 +36,7 @@ function onsignup(){
 
     list.appendChild(newList);
 
-    // localStorage.setItem('Name' ,document.getElementById('id1').value );
-    // localStorage.setItem('Email' ,document.getElementById('id2').value );
-    // localStorage.setItem('Phone' ,document.getElementById('id3').value );
-    // localStorage.setItem('Date' ,document.getElementById('id4').value );
-    // localStorage.setItem('Time' ,document.getElementById('id5').value );
-    // console.log(localStorage.getItem('Name'));
-    // console.log(localStorage.getItem('Email'));
-    // console.log(localStorage.getItem('Phone'));
-    // console.log(localStorage.getItem('Date'));
-    // console.log(localStorage.getItem('Time'));
-
-     }
-    //  let myObj={
-    //     name:'Sam',
-    //     age:'20'
-    //  }
-    //  let myObjSerialized=JSON.stringify(myObj);
-    //  localStorage.setItem('myObj' ,myObjSerialized);
-    //  console.log(localStorage.getItem('myObj'));
-    //  let myObjDeserialized=JSON.parse(localStorage.getItem('myObj'));
-    //  console.log(myObjDeserialized);
+}
 
 function removeElement(e){
     if(e.target.classList.contains('delete')){
